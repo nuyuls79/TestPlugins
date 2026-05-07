a1a6444cbe20d103c9dc14ac16c104d992c4f8dc
nuyuls79 <nuyulshare001@gmail.com>
2026-05-07 13:46:04 +0700

    Change language code from 'hi' to 'id'

--- Netmirror/src/main/kotlin/com/horis/cncverse/NetflixMirrorProvider.kt
+++ Netmirror/src/main/kotlin/com/horis/cncverse/NetflixMirrorProvider.kt
@@ -30,7 +30,7 @@ class NetflixMirrorProvider : MainAPI() {
         TvType.Anime,
         TvType.AsianDrama
     )
-    override var lang = "hi"
+    override var lang = "id"
 
     override var mainUrl = "https://net52.cc"
     private var newUrl = "https://net22.cc"
